b79a21210460fc29333b8bdd7256e6c00aea210c
odetv <abdiutama02@gmail.com>
2024-04-26 20:40:16 +0800

    add some components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Maintenance from "../components/Maintenance";
 import { maintenanceMode } from "../config/config";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Next-Gen Hydroponics",
@@ -20,7 +25,7 @@ export default function RootLayout({
   if (maintenanceMode) {
     return (
       <html lang="en">
-        <body className={inter.className}>
+        <body className={poppins.className}>
           <Maintenance />
         </body>
       </html>
@@ -29,7 +34,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
