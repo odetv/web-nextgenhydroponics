0bdfa26fb70894d5935fbb9eec214dee8dac8e01
odetv <abdiutama02@gmail.com>
2024-04-28 18:35:47 +0800

    change config firebase

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDA3-qjg56v0EprW_G8ONIA4Q6MQk1uyvI",
-  authDomain: "auth-nextgenhydroponics.firebaseapp.com",
-  projectId: "auth-nextgenhydroponics",
-  storageBucket: "auth-nextgenhydroponics.appspot.com",
-  messagingSenderId: "83010494154",
-  appId: "1:83010494154:web:4d91dde021c71ba836de33",
-  measurementId: "G-Y1JZV11JYX",
+  apiKey: "AIzaSyAcFafTyv0cWvjROU19MsXORdfsjPDixJI",
+  authDomain: "next-gen-hydroponics.firebaseapp.com",
+  projectId: "next-gen-hydroponics",
+  storageBucket: "next-gen-hydroponics.appspot.com",
+  messagingSenderId: "198680950116",
+  appId: "1:198680950116:web:f228d0a172d1ef6b34b671",
+  measurementId: "G-N8EJ00BKYL",
 };
 
 const app = initializeApp(firebaseConfig);
