2f4ade0b2e9e2da4a61942bd5674ed7b040053fb
odetv <abdiutama02@gmail.com>
2024-05-02 20:00:50 +0800

    update header and auth components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,15 @@ import {
   DropdownMenu,
   Avatar,
   Image,
-  NavbarBrand,
   NavbarContent,
   Tooltip,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Button,
+  useDisclosure,
+  Checkbox,
 } from "@nextui-org/react";
 import { SearchIcon } from "./SearchIcon";
 import DashboardIcon from "@mui/icons-material/Dashboard";
@@ -30,21 +36,8 @@ import app from "../../firebaseConfig";
 import { getAuth, User, signOut, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import AuthForm from "../components/AuthForm";
-
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-  Checkbox,
-} from "@nextui-org/react";
 import { MailIcon } from "./MailIcon";
 import { LockIcon } from "./LockIcon";
-import LoginIcon from "@mui/icons-material/Login";
 import { Divider } from "@mui/material";
 
 export default function Header() {
@@ -156,21 +149,19 @@ export default function Header() {
             type="search"
           />
         </Tooltip>
-
-        <Dropdown placement="bottom-start">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="success"
-              size="sm"
-              src={user?.photoURL || GuestIcon.src}
-              onClick={onOpen}
-            />
-          </DropdownTrigger>
-          <div className="font-semibold flex flex-row items-center justify-center">
-            {user ? (
+        {user ? (
+          <Dropdown placement="bottom-start">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="success"
+                size="sm"
+                src={user?.photoURL || GuestIcon.src}
+              />
+            </DropdownTrigger>
+            <div className="font-semibold flex flex-row items-center justify-center">
               <DropdownMenu aria-label="Profile Actions" variant="flat">
                 <DropdownItem
                   key="profile"
@@ -248,109 +239,119 @@ export default function Header() {
                   </NavbarContent>
                 </DropdownItem>
               </DropdownMenu>
-            ) : (
-              <Modal
-                isOpen={isOpen}
-                onOpenChange={onOpenChange}
-                placement="center"
-                backdrop="blur"
-              >
-                <ModalContent>
-                  {(onClose) => (
-                    <>
-                      <ModalHeader className="flex flex-col text-center">
-                        <p className="pt-4">Masuk Sekarang</p>
-                        <p className="pb-2 text-sm font-normal">
-                          Mulai Jelajahi Next-Gen Hydroponics
-                        </p>
-                      </ModalHeader>
-                      <ModalBody>
-                        <Input
-                          autoFocus
-                          endContent={
-                            <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                          }
-                          label="Email"
-                          placeholder="Email"
-                          variant="bordered"
-                        />
-                        <Input
-                          endContent={
-                            <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                          }
-                          label="Password"
-                          placeholder="Password"
-                          type="password"
-                          variant="bordered"
-                        />
-                        <div className="flex py-2 px-1 justify-between">
-                          <Checkbox
-                            classNames={{
-                              label: "text-small",
-                            }}
-                          >
-                            Ingat saya
-                          </Checkbox>
-                          <Link color="primary" href="#" size="sm">
-                            Lupa Password?
-                          </Link>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                          <Tooltip
-                            showArrow={true}
-                            size="sm"
-                            content="Maaf, Fitur belum tersedia."
-                            color="danger"
-                          >
-                            <button className="bg-emerald-700 w-full mx-auto rounded-lg py-2 text-white hover:bg-emerald-800 transition-all ease-in-out">
-                              Masuk
-                            </button>
-                          </Tooltip>
+            </div>
+          </Dropdown>
+        ) : (
+          <>
+            <Button
+              color="success"
+              variant="solid"
+              onClick={onOpen}
+              className="text-white font-medium"
+            >
+              Masuk
+            </Button>
+            <Modal
+              isOpen={isOpen}
+              onOpenChange={onOpenChange}
+              placement="center"
+              backdrop="blur"
+            >
+              <ModalContent>
+                {(onClose) => (
+                  <>
+                    <ModalHeader className="flex flex-col text-center">
+                      <p className="pt-4">Masuk Sekarang</p>
+                      <p className="pb-2 text-sm font-normal">
+                        Jadilah Bagian Next-Gen Hydroponics
+                      </p>
+                    </ModalHeader>
+                    <ModalBody>
+                      <Input
+                        autoFocus
+                        endContent={
+                          <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                        }
+                        label="Email"
+                        placeholder="Email"
+                        variant="bordered"
+                      />
+                      <Input
+                        endContent={
+                          <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                        }
+                        label="Password"
+                        placeholder="Password"
+                        type="password"
+                        variant="bordered"
+                      />
+                      <div className="flex py-2 px-1 justify-between">
+                        <Checkbox
+                          classNames={{
+                            label: "text-small",
+                          }}
+                        >
+                          Ingat saya
+                        </Checkbox>
+                        <Link color="primary" href="#" size="sm">
+                          Lupa Password?
+                        </Link>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <Tooltip
+                          showArrow={true}
+                          size="sm"
+                          content="Maaf, Fitur belum tersedia."
+                          color="danger"
+                        >
+                          <button className="bg-emerald-700 w-full mx-auto rounded-lg py-2 text-white hover:bg-emerald-800 transition-all ease-in-out">
+                            Masuk
+                          </button>
+                        </Tooltip>
 
-                          <Divider className="text-xs">Atau</Divider>
-                          <div className="flex flex-col gap-6">
-                            <div className="flex items-center justify-center w-full mx-auto">
-                              <button
-                                onClick={signInWithGoogle}
-                                className="justify-center w-full mx-auto px-4 py-2 border flex gap-2 items-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-300 hover:bg-slate-100 transition-all ease-in-out"
+                        <Divider className="text-xs">Atau</Divider>
+                        <div className="flex flex-col gap-6">
+                          <div className="flex items-center justify-center w-full mx-auto">
+                            <button
+                              onClick={signInWithGoogle}
+                              className="justify-center w-full mx-auto px-4 py-2 border flex gap-2 items-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-300 hover:bg-slate-100 transition-all ease-in-out"
+                            >
+                              <Image
+                                className="w-6 h-6"
+                                src="https://www.svgrepo.com/show/475656/google-color.svg"
+                                loading="lazy"
+                                alt="google logo"
+                                width={24}
+                                height={24}
+                              />
+                              <p className="text-sm">Login with Google</p>
+                            </button>
+                          </div>
+                          <div className="flex flex-row gap-1 justify-center pb-6">
+                            <p className="text-sm">Belum punya akun?</p>
+                            <Tooltip
+                              showArrow={true}
+                              size="sm"
+                              content="Maaf, Fitur belum tersedia."
+                              color="danger"
+                            >
+                              <a
+                                href="#"
+                                className="text-sm font-semibold text-emerald-700 hover:text-emerald-800 transition-all ease-in-out"
                               >
-                                <Image
-                                  className="w-6 h-6"
-                                  src="https://www.svgrepo.com/show/475656/google-color.svg"
-                                  loading="lazy"
-                                  alt="google logo"
-                                  width={24}
-                                  height={24}
-                                />
-                                <p className="text-sm">Login with Google</p>
-                              </button>
-                            </div>
-                            <div className="flex flex-row gap-1 justify-center pb-6">
-                              <p className="text-sm">Belum punya akun?</p>
-                              <Tooltip
-                                showArrow={true}
-                                size="sm"
-                                content="Maaf, Fitur belum tersedia."
-                                color="danger"
-                              >
-                                <a
-                                  href="#"
-                                  className="text-sm font-semibold text-emerald-700 hover:text-emerald-800 transition-all ease-in-out"
-                                >
-                                  Daftar Sekarang
-                                </a>
-                              </Tooltip>
-                            </div>
+                                Daftar Sekarang
+                              </a>
+                            </Tooltip>
                           </div>
                         </div>
-                      </ModalBody>
-                    </>
-                  )}
-                </ModalContent>
-              </Modal>
-            )}
-          </div>
-        </Dropdown>
+                      </div>
+                    </ModalBody>
+                  </>
+                )}
+              </ModalContent>
+            </Modal>
+          </>
+        )}
       </div>
     </Navbar>
   );
