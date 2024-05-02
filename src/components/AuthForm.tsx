4a1b15999ca8267c7f6c1355da47254a749a26c0
odetv <abdiutama02@gmail.com>
2024-05-02 21:50:48 +0800

    update auth components

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -77,6 +77,7 @@ export default function App() {
         backdrop="blur"
         scrollBehavior="outside"
         shadow="lg"
+        className="mr-4 ml-4"
       >
         <ModalContent>
           {(onClose) => (
