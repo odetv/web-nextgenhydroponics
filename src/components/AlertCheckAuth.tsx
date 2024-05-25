946cfa31f93537af0d5983d7a6248a77350e03c0
odetv <abdiutama02@gmail.com>
2024-05-25 20:57:35 +0800

    update components

--- src/components/AlertCheckAuth.tsx
+++ src/components/AlertCheckAuth.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@nextui-org/react";
 
 export default function AlertCheckAuth() {
   return (
-    <div className="flex flex-col justify-center items-center text-center min-h-screen mx-auto p-4">
+    <div className="flex flex-col justify-center items-center text-center min-h-screen mx-auto p-4 -mt-16">
       <Spinner label="Memeriksa autentikasi..." color="success" />
     </div>
   );
