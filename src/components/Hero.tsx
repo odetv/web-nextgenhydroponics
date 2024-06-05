79f12e13f661c9d176089fcdb1a12edfe12fa594
odetv <abdiutama02@gmail.com>
2024-06-05 10:53:33 +0800

    chore: Update framer-motion dependency to version 11.2.10 and add ParallaxText component for scroll animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,6 +9,79 @@ import { TypeAnimation } from "react-type-animation";
 import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from "framer-motion";
+import { wrap } from "@motionone/utils";
+import { useRef } from "react";
+
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  /**
+   * This is a magic wrapping for the length of the text - you
+   * have to replace for wrapping that works for you or dynamically
+   * calculate
+   */
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    /**
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
+     */
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
+  return (
+    <div className="overflow-hidden tracking-tight leading-normal m-0 whitespace-nowrap flex flex-nowrap">
+      <motion.div className="flex whitespace-nowrap flex-nowrap" style={{ x }}>
+        <span className="block mr-1">{children} </span>
+        <span className="block mr-1">{children} </span>
+        <span className="block mr-1">{children} </span>
+        <span className="block mr-1">{children} </span>
+      </motion.div>
+    </div>
+  );
+}
 
 export default function Hero() {
   return (
@@ -108,9 +181,14 @@ export default function Hero() {
             </Carousel>
           </div>
           <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-            <p className="text-tiny text-white/80 p-2 text-center">
-              Tim Smart Green Garden
-            </p>
+            <div className="text-tiny text-white/80 p-2 text-center">
+              <ParallaxText baseVelocity={-1}>
+                Tim Smart Green Garden Pertanian Canggih Cerdas Keren Internet
+                of Things Artificial Intelligence Energi Terbarukan Panel Surya
+                - Kerjasama Fakultas Teknik dan Kejuruan Undiksha dengan PT.
+                Dago Engineering |
+              </ParallaxText>
+            </div>
           </CardFooter>
         </Card>
       </div>
