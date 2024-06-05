c4608a9d3aa03a4d2e33c5cd0b4b8deeb71e0d33
odetv <abdiutama02@gmail.com>
2024-06-05 10:14:07 +0800

    chore: Update framer-motion dependency to version 11.2.10 and add useSpring hook for scroll animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,15 @@ import Collaboration from "@/components/Collaboration";
 import Team from "@/components/Team";
 import Features from "@/components/Features";
 import Contact from "@/components/Contact";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function LandingPage() {
   const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
   return (
     <main
@@ -17,7 +22,7 @@ export default function LandingPage() {
     >
       <motion.div
         className="progress-bar fixed top-16 left-0 right-0 h-1 origin-left bg-green-500 z-50"
-        style={{ scaleX: scrollYProgress }}
+        style={{ scaleX }}
       />
       <Hero />
       <Collaboration />
