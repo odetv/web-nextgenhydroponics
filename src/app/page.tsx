ab95b6a10177b96f38a403f1b7055530b5fd734f
odetv <abdiutama02@gmail.com>
2024-06-05 10:07:23 +0800

    chore: Update framer-motion dependency to version 11.2.10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,20 @@ import Collaboration from "@/components/Collaboration";
 import Team from "@/components/Team";
 import Features from "@/components/Features";
 import Contact from "@/components/Contact";
+import { motion, useScroll } from "framer-motion";
 
 export default function LandingPage() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <main
       id="beranda"
       className="flex flex-col justify-center items-center min-h-screen pb-16 pt-4 sm:pt-16"
     >
+      <motion.div
+        className="progress-bar fixed top-16 left-0 right-0 h-1 origin-left bg-green-500 z-50"
+        style={{ scaleX: scrollYProgress }}
+      />
       <Hero />
       <Collaboration />
       <Team />
