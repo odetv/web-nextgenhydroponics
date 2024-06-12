3eda94b8d8a89320eea51fa047a6b1405b1270cf
odetv <abdiutama02@gmail.com>
2024-06-12 11:45:51 +0800

    chore: Update help page layout and accordion component

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -16,7 +16,7 @@ export default function Help() {
       </div>
       <div>
         <p className="font-bold text-3xl text-center">
-          Bantuan dan Pertanyaan Umum
+          Bantuan Pertanyaan Umum
         </p>
       </div>
       <div>
@@ -30,26 +30,28 @@ export default function Help() {
           matahari menggunakan panel surya.
         </p>
       </div>
-      <Accordion variant="shadow">
-        <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="4" aria-label="Accordion 4" title="Accordion 4">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="5" aria-label="Accordion 5" title="Accordion 5">
-          {defaultContent}
-        </AccordionItem>
-        <AccordionItem key="6" aria-label="Accordion 6" title="Accordion 6">
-          {defaultContent}
-        </AccordionItem>
-      </Accordion>
+      <div className="text-sm w-full mt-2 bg-gray-100 rounded-lg p-2">
+        <Accordion isCompact>
+          <AccordionItem key="1" aria-label="Pertanyaan 1" title="Pertanyaan 1">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="2" aria-label="Pertanyaan 2" title="Pertanyaan 2">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="3" aria-label="Pertanyaan 3" title="Pertanyaan 3">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="4" aria-label="Pertanyaan 4" title="Pertanyaan 4">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="5" aria-label="Pertanyaan 5" title="Pertanyaan 5">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="6" aria-label="Pertanyaan 6" title="Pertanyaan 6">
+            {defaultContent}
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }
