57dad73e2eff861cdc1c10cc9acbd7bf28534696
odetv <abdiutama02@gmail.com>
2024-07-06 08:50:48 +0800

    refactor: Add lazy loading for images and fetch latest data for SpedoAirHidroponik component

--- src/components/SpedoAirHidroponik.tsx
+++ src/components/SpedoAirHidroponik.tsx
@@ -1,7 +1,69 @@
 import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+import { limitToLast, onValue, query, ref } from "firebase/database";
 import React, { useState, useEffect } from "react";
+import { database } from "../../firebaseConfig";
 
 export default function SpedoAirHidroponik() {
+  const [kapasitasTandonPencampuran, setKapasitasTandonPencampuran] = useState<
+    string | null
+  >(null);
+
+  useEffect(() => {
+    const esp32InfoRef = ref(database, "esp32info");
+    const latestQuery = query(esp32InfoRef, limitToLast(1));
+
+    onValue(latestQuery, (latestSnapshot) => {
+      latestSnapshot.forEach((latestDateSnapshot) => {
+        latestDateSnapshot.forEach((latestTimeSnapshot) => {
+          const latestData = latestTimeSnapshot.val();
+          const latestKapasitasTandonPencampuran =
+            latestData.kapasitas_tandon_pencampuran;
+
+          if (
+            latestKapasitasTandonPencampuran !== null &&
+            latestKapasitasTandonPencampuran >= 0 &&
+            latestKapasitasTandonPencampuran <= 61
+          ) {
+            const jarakSensorKeTutupTandon = 20; // Misalkan 20 cm dari sensor ke tutup tandon
+            const tinggiTandon = 41; // Tinggi tandon adalah 41 cm
+
+            // Hitung tinggi air dari dasar tandon
+            const tinggiAir =
+              tinggiTandon -
+              (latestKapasitasTandonPencampuran - jarakSensorKeTutupTandon);
+
+            // Pastikan tinggi air tidak melebihi tinggi tandon
+            const tinggiAirValid = Math.max(
+              0,
+              Math.min(tinggiAir, tinggiTandon)
+            );
+
+            // Hitung kapasitas dalam liter
+            const kapasitasTandonPencampuranInLiters =
+              (tinggiAirValid / tinggiTandon) * 150;
+
+            // Membulatkan ke dua desimal
+            const kapasitasTandonPencampuranInLitersRounded =
+              kapasitasTandonPencampuranInLiters.toFixed(1);
+
+            // Konversi ke string sebelum diset
+            setKapasitasTandonPencampuran(
+              kapasitasTandonPencampuranInLitersRounded
+            );
+          } else if (latestKapasitasTandonPencampuran > 61) {
+            // Jika nilai > 61 cm, set kapasitas menjadi 0 liter
+            setKapasitasTandonPencampuran("0");
+          } else {
+            console.error(
+              "Invalid latestKapasitasTandonPencampuran value:",
+              latestKapasitasTandonPencampuran
+            );
+          }
+        });
+      });
+    });
+  }, []);
+
   return (
     <div className="">
       <p className="text-sm -mb-6 text-center">Tandon Air Hidroponik</p>
@@ -10,7 +72,7 @@ export default function SpedoAirHidroponik() {
         endAngle={110}
         width={172}
         height={172}
-        value={50}
+        value={Number(kapasitasTandonPencampuran)}
         valueMin={0}
         valueMax={150}
         sx={(theme) => ({
